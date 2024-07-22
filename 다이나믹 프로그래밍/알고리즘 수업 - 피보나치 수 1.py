2ad39e0905ba50b2bf7dc6dd7e2075efffcae773
soyoon1 <20210841@duksung.ac.kr>
2024-07-22 23:07:57 +0900

    [0722] 다이나믹 프로그래밍 문제

---
+++ 다이나믹 프로그래밍/알고리즘 수업 - 피보나치 수 1.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+def fib(n):
+  a, b = 1, 1 # f(1), f(2)
+  for i in range(3, n+1): # 1, 2는 이미 정의했으므로 3부터 n까지
+    a, b = b, a+b
+
+  return b
+
+def fibonacci(n):
+  return n-2
+
+print(fib(n), fibonacci(n))
