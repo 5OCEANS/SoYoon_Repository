fc49567b80464f3fb4219dfab4f92a0c89252c9d
soyoon1 <20210841@duksung.ac.kr>
2024-08-30 22:14:09 +0900

    [0830] 사칙연산 문제

---
+++ 사칙연산/Julka.py
@@ -0,0 +1,9 @@
+import sys
+
+totalCount = int(sys.stdin.readline())
+moreKlaudia = int(sys.stdin.readline())
+
+natalia = (totalCount - moreKlaudia) // 2
+
+print(natalia + moreKlaudia)
+print(natalia)
