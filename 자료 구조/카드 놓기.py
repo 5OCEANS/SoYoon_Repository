7a5e5798ff3313d7982349b153bf0b7e53f28eb0
soyoon1 <20210841@duksung.ac.kr>
2024-09-28 19:58:15 +0900

    [0928] 자료 구조 문제

---
+++ 자료 구조/카드 놓기.py
@@ -0,0 +1,36 @@
+# 재귀함수 사용
+# import sys
+
+# n = int(sys.stdin.readline())
+# k = int(sys.stdin.readline())
+
+# cards = [sys.stdin.readline().strip() for _ in range(n)]
+# cards_list = []
+
+# def pick(result, n, picked):
+#   if n == k:
+#     if result not in cards_list:
+#       cards_list.append(result)
+#     return
+
+#   for card_idx in range(len(cards)):
+#     if card_idx not in picked:
+#       picked.append(card_idx)
+#       new_result = result + cards[card_idx]
+#       pick(new_result, n+1, picked)
+#       picked.pop()
+
+# pick("", 0, [])
+# print(len(cards_list))
+
+
+
+# itertools 사용
+import itertools
+import sys
+
+n = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
+
+cards = [sys.stdin.readline().strip() for _ in range(n)]
+print(len(set("".join(i) for i in itertools.permutations(cards, k))))
