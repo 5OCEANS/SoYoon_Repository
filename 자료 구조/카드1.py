c2fae3d61de50332459fe7ab43248d0553b35de1
soyoon1 <20210841@duksung.ac.kr>
2024-10-06 00:00:41 +0900

    [1006] 자료 구조 문제

--- 자료 구조/카드1.py
+++ 자료 구조/카드1.py
@@ -8,3 +8,16 @@ for i in range(N-1):
   numList.insert(0, numList.pop())
 
 print(numList[0])
+
+
+N = int(input())
+card_list = [i for i in range(1, N+1)]
+discarded_card = []
+
+while len(card_list) != 1:
+    discarded_card.append(card_list.pop(0)) #버리기
+    card_list.append(card_list.pop(0)) #뒤로 옮기기
+
+for i in discarded_card:
+    print(i, end = ' ')
+print(card_list[0])
