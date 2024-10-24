6ecfdc72554a7fbabdbf34a6ea69a796eb83b034
soyoon1 <20210841@duksung.ac.kr>
2024-10-24 23:48:40 +0900

    [1024] 기하학 문제

---
+++ 기하학/Pizza.py
@@ -0,0 +1,12 @@
+import sys, math
+
+n = int(sys.stdin.readline().strip())
+
+for i in range(n):
+  a1, p1 = map(int, sys.stdin.readline().strip().split())
+  r1, p2 = map(int, sys.stdin.readline().strip().split())
+
+  if (a1/p1) > ((math.pi*r1*r1)/p2):
+    print('Slice of pizza')
+  else:
+    print('Whole pizza')
