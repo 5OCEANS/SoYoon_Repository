3a38083de73c45e302fcfb828946920db718462d
soyoon1 <20210841@duksung.ac.kr>
2025-03-17 23:58:24 +0900

    [0317] 마라톤 문제

---
+++ 마라톤/코스 41/N과 M (12).py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().strip().split())
+k = sorted(set(list(map(int, sys.stdin.readline().strip().split()))))
+ans = []
+p = []
+def solve(depth, idx):
+  if depth == m:
+    print(' '.join(map(str, ans)))
+    return
+
+  for i in range(idx, len(k)):
+    ans.append(k[i])
+    solve(depth+1, i)
+    ans.pop()
+
+solve(0, 0)
+p = sorted(list(set(p)))
