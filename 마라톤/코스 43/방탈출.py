58d164cfe1b4bce3171fd6d7393c665e732b86e9
soyoon1 <20210841@duksung.ac.kr>
2025-04-01 23:29:27 +0900

    [0401] 마라톤 문제

---
+++ 마라톤/코스 43/방탈출.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+num = list(map(int, sys.stdin.readline().strip().split()))
+current = [0] * N
+
+cnt = 0
+for i in range(N):
+  if num[i] != current[i]:
+    cnt += 1
+    current[i] = not current[i]
+
+    if i + 1 < N:
+      current[i+1] = not current[i+1]
+    if i + 2 < N:
+      current[i+2] = not current[i+2]
+
+print(cnt)
